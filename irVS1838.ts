e2237bebf73909a7f64880d0c6a1af7bb62b874d
thireyes <thireyes@qq.com>
2022-07-16 10:49:12 +0800

    v0.0.11

--- irVS1838.ts
+++ irVS1838.ts
@@ -9,7 +9,7 @@ const enum IrButtonAction {
 }
 
 // *************************************************** [NAMESPACE] IR VS1838 ****************************************** //
-//% weight=95 color=#bb0033 icon="\uf09e" block="C33 IR VS1838"
+//% weight=95 color=#bb0033 icon="\uf09e" block="C33 IR Receiver"
 //% category="C33 IR VS1838"
 namespace irVS1838 {
   // * * * * * * * * * * * * * * * * * * * * * * * * * * declarations * * * * * * * * * ** * * * * * * * * * * * //
